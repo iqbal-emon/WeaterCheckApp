c608a92a9e0639cb1f2c1fea591d9068f2d9b4f8
Emon <ihemon281@gmail.com>
2023-10-03 22:22:02 +0600

    after add country details

--- weather.js
+++ weather.js
@@ -1,10 +1,21 @@
 const apikey = "c356f02fdd470ffd2d98a64ac6e539b0";
 const apiUrl ="https://api.openweathermap.org/data/2.5/weather?&units=metric";
 
+const apiUrl2 = "https://restcountries.com/v3.1/capital/";
 
 var search = document.getElementById("Search");
 var ipt = document.getElementById("ipt");
 var img = document.getElementById("ih");
+var country = document.getElementById("country");
+var region = document.getElementById("region");
+var img2 = document.getElementById("img2");
+
+
+// var language = document.getElementById("language");
+
+
+
+
 
 search.addEventListener("click", () => {
     
@@ -13,11 +24,21 @@ search.addEventListener("click", () => {
 
 });
 
+
+
+
+
+
 async function checkWeather(city) {
   
     const response = await fetch(apiUrl + '&q=' + city + `&appid=${apikey}`);
-    var data = await response.json();
-    console.log(data);
+  var data = await response.json();
+  
+const response2 = await fetch(apiUrl2+city);
+  var data2 = await response2.json();
+  console.log(data2);
+
+
     document.getElementById("degree").innerHTML = Math.round(data.main.temp) + "°C";
     
     document.getElementById("name").innerHTML = data.name;
@@ -45,7 +66,29 @@ async function checkWeather(city) {
   }
    if (data.weather[0].main == "Haze") {
      img.src = "./images/haze.png";
-   }
+  }
+  document.getElementById("img2").src = data2[0].flags.png;
+  
+ document.getElementById("country").innerHTML ="Country :" + data2[0].name.common;
+
+    
+  document.getElementById("region").innerHTML ="Region :" + data2[0].region;
+
+
+  var populationInMillion = parseFloat(data2[0].population) / 1000000;
+ console.log(populationInMillion)
+
+    document.getElementById("language").innerHTML ="Population:"+  populationInMillion;
+  
+    
+    //  document.getElementById("humidity").innerHTML = data.main.humidity + "%";
+    // document.getElementById("wind").innerHTML = data.wind.speed + "km/h";
+
+
+
+
+
+
 
 
 
